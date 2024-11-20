2af1920f507a74516343aa01df457e7b60620505
ramazanusen <ramazanusen@hotmail.com>
2024-11-21 01:55:22 +0300

    Initial Commit

---
+++ MovieRecommender/MovieRecommenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieRecommenderApp.swift
+//  MovieRecommender
+//
+//  Created by Ramazan Üsen on 21.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieRecommenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieRecommenderTests/MovieRecommenderTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieRecommenderTests.swift
+//  MovieRecommenderTests
+//
+//  Created by Ramazan Üsen on 21.11.2024.
+//
+
+import Testing
+@testable import MovieRecommender
+
+struct MovieRecommenderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
